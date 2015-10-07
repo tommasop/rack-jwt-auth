d42c6966242007c603f1ec7fcf10b38436e4e20d
Tommaso Patrizi <tommasop@gmail.com>
2015-10-07 11:09:47 +0200

    no payload

--- lib/rack/jwt/auth/authenticate.rb
+++ lib/rack/jwt/auth/authenticate.rb
@@ -48,7 +48,7 @@ module Rack
             return [401, {}, [{message: 'Invalid Authorization'}.to_json]] unless payload
           end
 
-          yield payload[0]
+          yield payload
         end
 
         def compile_paths(paths)
